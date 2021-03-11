ceb194c43e843658d5428d64fcc37322b2ead789
WinnieThePoohDev <79548090+WinnieThePoohDev@users.noreply.github.com>
2021-03-11 19:54:01 +0000

    Add files via upload

---
+++ xiFarm.sol
@@ -0,0 +1,449 @@
+pragma solidity ^0.5.16;
+
+interface IBEP20 {
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
+
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external view returns (uint8);
+
+  /**
+   * @dev Returns the token symbol.
+   */
+  function symbol() external view returns (string memory);
+
+  /**
+  * @dev Returns the token name.
+  */
+  function name() external view returns (string memory);
+
+  /**
+   * @dev Returns the bep token owner.
+   */
+  function getOwner() external view returns (address);
+
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
+
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Returns the remaining number of tokens that `spender` will be
+   * allowed to spend on behalf of `owner` through {transferFrom}. This is
+   * zero by default.
+   *
+   * This value changes when {approve} or {transferFrom} are called.
+   */
+  function allowance(address _owner, address spender) external view returns (uint256);
+
+  /**
+   * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * IMPORTANT: Beware that changing an allowance with this method brings the risk
+   * that someone may use both the old and the new allowance by unfortunate
+   * transaction ordering. One possible solution to mitigate this race
+   * condition is to first reduce the spender's allowance to 0 and set the
+   * desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * Emits an {Approval} event.
+   */
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Moves `amount` tokens from `sender` to `recipient` using the
+   * allowance mechanism. `amount` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Emitted when `value` tokens are moved from one account (`from`) to
+   * another (`to`).
+   *
+   * Note that `value` may be zero.
+   */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with GSN meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+contract Context {
+  // Empty internal constructor, to prevent people from mistakenly deploying
+  // an instance of this contract, which should be used via inheritance.
+  constructor () internal { }
+
+  function _msgSender() internal view returns (address payable) {
+    return msg.sender;
+  }
+
+  function _msgData() internal view returns (bytes memory) {
+    this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    return msg.data;
+  }
+}
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+  /**
+   * @dev Returns the addition of two unsigned integers, reverting on
+   * overflow.
+   *
+   * Counterpart to Solidity's `+` operator.
+   *
+   * Requirements:
+   * - Addition cannot overflow.
+   */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the subtraction of two unsigned integers, reverting on
+   * overflow (when the result is negative).
+   *
+   * Counterpart to Solidity's `-` operator.
+   *
+   * Requirements:
+   * - Subtraction cannot overflow.
+   */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
+
+  /**
+   * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+   * overflow (when the result is negative).
+   *
+   * Counterpart to Solidity's `-` operator.
+   *
+   * Requirements:
+   * - Subtraction cannot overflow.
+   */
+  function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the multiplication of two unsigned integers, reverting on
+   * overflow.
+   *
+   * Counterpart to Solidity's `*` operator.
+   *
+   * Requirements:
+   * - Multiplication cannot overflow.
+   */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the integer division of two unsigned integers. Reverts on
+   * division by zero. The result is rounded towards zero.
+   *
+   * Counterpart to Solidity's `/` operator. Note: this function uses a
+   * `revert` opcode (which leaves remaining gas untouched) while Solidity
+   * uses an invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   * - The divisor cannot be zero.
+   */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return div(a, b, "SafeMath: division by zero");
+  }
+
+  /**
+   * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+   * division by zero. The result is rounded towards zero.
+   *
+   * Counterpart to Solidity's `/` operator. Note: this function uses a
+   * `revert` opcode (which leaves remaining gas untouched) while Solidity
+   * uses an invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   * - The divisor cannot be zero.
+   */
+  function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    // Solidity only automatically asserts when dividing by 0
+    require(b > 0, errorMessage);
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+   * Reverts when dividing by zero.
+   *
+   * Counterpart to Solidity's `%` operator. This function uses a `revert`
+   * opcode (which leaves remaining gas untouched) while Solidity uses an
+   * invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   * - The divisor cannot be zero.
+   */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    return mod(a, b, "SafeMath: modulo by zero");
+  }
+
+  /**
+   * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+   * Reverts with custom message when dividing by zero.
+   *
+   * Counterpart to Solidity's `%` operator. This function uses a `revert`
+   * opcode (which leaves remaining gas untouched) while Solidity uses an
+   * invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   * - The divisor cannot be zero.
+   */
+  function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+    require(b != 0, errorMessage);
+    return a % b;
+  }
+}
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+  address private _owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  /**
+   * @dev Initializes the contract setting the deployer as the initial owner.
+   */
+  constructor () internal {
+    address msgSender = _msgSender();
+    _owner = msgSender;
+    emit OwnershipTransferred(address(0), msgSender);
+  }
+
+  /**
+   * @dev Returns the address of the current owner.
+   */
+  function owner() public view returns (address) {
+    return _owner;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(_owner == _msgSender(), "Ownable: caller is not the owner");
+    _;
+  }
+
+  /**
+   * @dev Leaves the contract without owner. It will not be possible to call
+   * `onlyOwner` functions anymore. Can only be called by the current owner.
+   *
+   * NOTE: Renouncing ownership will leave the contract without an owner,
+   * thereby removing any functionality that is only available to the owner.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  /**
+   * @dev Transfers ownership of the contract to a new account (`newOwner`).
+   * Can only be called by the current owner.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  /**
+   * @dev Transfers ownership of the contract to a new account (`newOwner`).
+   */
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0), "Ownable: new owner is the zero address");
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+
+
+
+//XI FARM START
+
+contract XiFarm is Context, Ownable {
+
+  using SafeMath for uint256;
+
+  address poohAddy = 0x9673C2196fCAe71bE87864CDb04aDc4644559e89;//POOHTEST ADDY
+  address XiAddy = 0x098b246bf19ba9C5aD9C5dD994815e1db444eaA6;//XITEST ADDY
+
+  IBEP20 POOH = IBEP20(poohAddy);
+  IBEP20 Xi = IBEP20(XiAddy);
+
+  uint public xiStakeRewardCap = 20000000 * (10 ** 18); //20% of TOTAL Xi Supply
+  uint public xiStakeTime = 96000; //TIME IN BLOCKS
+
+  uint public blockReward = (xiStakeRewardCap/xiStakeTime) * (10 ** 18);
+  uint totalStaked = 0;
+
+  address[] public stakers;
+
+  mapping (address => uint256) public stakedBalance;
+  mapping (address => bool) public isStaking;
+
+
+  constructor() public {
+  }
+    //Struct that keeps information about each users stake, mapped to with Stakes
+    struct stakeInfo {
+
+        uint[] deposits;
+        uint[] depositTimes;
+        uint[] rewards;
+    }
+
+    mapping(address => stakeInfo) Stakes;
+
+    function stakePOOH(uint _amount) public {
+
+        require(_amount > 0);
+
+        //Transfer POOH for staking
+        POOH.transferFrom(msg.sender, address(this), _amount);
+        //Update stakedBalance for msgsender address
+        stakedBalance[msg.sender] = stakedBalance[msg.sender].add(_amount);
+        //Add _amount to totalStaked
+        totalStaked = totalStaked.add(_amount);
+        //Push _amount to deposits[] and block.number to depositTimes[]
+        //Used to calculate poolShare and distribute rewards upon unstaking - repeated for every deposit made individually
+        Stakes[msg.sender].deposits.push(_amount);
+        Stakes[msg.sender].depositTimes.push(block.number);
+
+
+    }
+
+    //OLD SHITTY FUNCTION
+    /*function unstakeAndClaim() public {
+        //Ensure sender is staking and has balance above zero
+        require(stakedBalance[msg.sender] > 0);
+        //Get stakeStartBlock and current block
+        uint startBlock = stakeStartBlock[msg.sender];
+        //Store difference in block numbers under "diff" variable
+        uint diff = (getAverageBlock(msg.sender) - startBlock);
+        //Calculate what % of the pool the user is staking
+        uint poolShare = (stakedBalance[msg.sender]/totalStaked).mul(100);
+        //Give them this % of the blockReward multiplied by the number of blocks they staked for (diff) in Xi
+        uint rewardAmountXi = ((poolShare*blockReward)/100).mul(diff);
+        //Set staking flag to false, set startStakeBlock to 0
+        //Reduces totalStaked value by user stakeAmount
+        //NOTE: Unstaking will reset users block count.
+        totalStaked = totalStaked.sub(stakedBalance[msg.sender]);
+        isStaking[msg.sender] = false;
+        stakeStartBlock[msg.sender] = 0;
+        //Transfer staked POOH back to user, and transfer correct amount of Xi
+        POOH.transfer(msg.sender, stakedBalance[msg.sender]);
+        //remove stakedBalance balance
+        stakedBalance[msg.sender] = 0;
+        Xi.transfer(msg.sender, rewardAmountXi);
+    }*/
+
+    //NEW SHINY AND IMPROVED, MAYBE, A BIT? WHO KNOWS?! FUNCTION
+    function unstakeTokens() public {
+        //Check staked balance above 0
+        require(stakedBalance[msg.sender] > 0);
+        //Repeat for every deposit made by user
+        for(uint i; i < Stakes[msg.sender].deposits.length; i++) {
+            //Calculate pool share % of deposit each time
+            uint poolShare = (Stakes[msg.sender].deposits[i]/totalStaked).mul(100);
+            //get difference in block numbers from deposit block to current block
+            uint diff = (block.number.sub(Stakes[msg.sender].depositTimes[i]));
+            //assign appropriate reward to user rewards[i]
+            Stakes[msg.sender].rewards[i] = ((poolShare*blockReward)/100).mul(diff);
+            //Subtract deposit value from total staked value
+            totalStaked = totalStaked.sub(Stakes[msg.sender].deposits[i]);
+            //send amount of Xi stored in rewards[i] to user
+            Xi.transfer(msg.sender, Stakes[msg.sender].rewards[i]);
+            //subtract deposits[i] from user stakedBalance
+            stakedBalance[msg.sender] = stakedBalance[msg.sender].sub(Stakes[msg.sender].deposits[i]);
+        }
+        //delete deposits[] and depositTimes[] to clear??
+        delete Stakes[msg.sender].deposits;
+        delete Stakes[msg.sender].depositTimes;
+        //Transfer POOH back to user
+        POOH.transfer(msg.sender, stakedBalance[msg.sender]);
+        //Set user stakedBalance back to 0
+        stakedBalance[msg.sender] = 0;
+    }
+
+
+}
