c08f9037c74109e44425717d0555055b1ea7b43c
WinnieThePoohDev <79548090+WinnieThePoohDev@users.noreply.github.com>
2021-03-31 22:14:43 +0100

    Update xiFarm.sol
    tried to pre-emptively add LP staking but so far can only work it correctly if LP stakes are locked for the full duration.

--- xiFarm.sol
+++ xiFarm.sol
@@ -12,25 +12,36 @@ contract XiFarm is Context, Ownable {
   
   address poohAddy = 0x9673C2196fCAe71bE87864CDb04aDc4644559e89;//POOHTEST ADDY
   address XiAddy = 0x098b246bf19ba9C5aD9C5dD994815e1db444eaA6;//XITEST ADDY
+  address LPaddy = 0x0000000000000000000000000000000000000000; //LPTokenAddy - to be updated with onlyOwner function
   
   IBEP20 POOH = IBEP20(poohAddy);
   IBEP20 Xi = IBEP20(XiAddy);
+  IBEP20 LPtoken = IBEP20(LPaddy);
   
   uint public xiStakeRewardCap = 20000000 * (10 ** 18);//20% of TOTAL Xi Supply - To be deposited by Xi Token contract creator
   uint public xiStakeTime = 96000; //TIME IN BLOCKS - ~1,440,000 seconds
   uint public xiTokensLeft; // Tracks how many REWARD tokens are left in contract
+  uint public LPStakeRewardCap = 70000000 * (10 ** 18); // 70% of TOTAL Xi supply
+  uint public LPStakeTime = 192000; // approx 4 weeks in blocks
+  uint public LPstakeTimegate = 13000; //approx 2 days in blocks
  
   uint totalStaked = 0;
   uint startBlock;
+  uint LPstartBlock;
   uint endBlock;
+  uint LPendBlock;
+  uint totalLPStaked = 0;
   
   mapping (address => uint256) public stakedBalance;
+  mapping (address => uint256) public LPstakedBalance;
 
 
   constructor() public {
-      xiTokensLeft = xiStakeRewardCap;
+      xiTokensLeft = xiStakeRewardCap+LPStakeRewardCap;
       startBlock = block.number;
+      LPstartBlock = block.number+LPstakeTimegate;
       endBlock = startBlock+xiStakeTime;
+      LPendBlock = LPstartBlock+LPStakeTime;
   }
     //Struct that keeps information about each users stake, mapped to with Stakes
     struct stakeInfo {
@@ -41,7 +52,17 @@ contract XiFarm is Context, Ownable {
         
     }
     
+    //Struct that keeps information about each users stake, mapped to with LPStakes
+    struct LPstakeInfo {
+        
+        uint[] deposits;
+        uint[] depositTimes;
+        uint[] rewards;
+        
+    }
+    
     mapping(address => stakeInfo) Stakes;
+    mapping(address => stakeInfo) LPStakes;
     
     function stakePOOH(uint256 _amount) public {
         require(_amount > 0);
@@ -59,7 +80,7 @@ contract XiFarm is Context, Ownable {
 
     }
     
-    function unstakeTokens() public {
+    function unstakePOOH() public {
         //Check staked balance above 0
         require(stakedBalance[msg.sender] > 0);
         //Repeat for every deposit made by user
@@ -71,7 +92,7 @@ contract XiFarm is Context, Ownable {
                 diff = diff.sub(block.number-endBlock);
             }
             //assign appropriate reward to user rewards[i]
-            //Number of tokens staked multiplied by number of blocks staked for and divided by 20k cos im shit at math and can't make it work any other way
+            //Number of tokens staked multiplied by number of blocks staked for and divided by 20k to keep the total supply reasonable
             Stakes[msg.sender].rewards[i] = (Stakes[msg.sender].deposits[i].mul(diff))/20000;
             //send amount of Xi stored in rewards[i] to user
             Xi.transfer(msg.sender, Stakes[msg.sender].rewards[i]);
@@ -92,8 +113,75 @@ contract XiFarm is Context, Ownable {
         
     }
     
+    function stakeLP(uint256 _amount) public {
+        require(_amount > 0);
+        require(block.number < LPendBlock);
+        //Transfer POOH for staking
+        require(LPtoken.transferFrom(msg.sender, address(this), _amount));
+        //Update stakedBalance for msgsender address
+        LPstakedBalance[msg.sender] = LPstakedBalance[msg.sender].add(_amount);
+        //Add _amount to totalStaked
+        totalLPStaked = totalLPStaked.add(_amount);
+        //Push _amount to deposits[] and block.number to depositTimes[]
+        //Used to calculate and distribute rewards upon unstaking - repeated for every deposit made individually
+        LPStakes[msg.sender].deposits.push(_amount);
+        LPStakes[msg.sender].depositTimes.push(block.number);
+
+    }
+    
+    function claimLPstakes() public {
+        require(block.number >= LPendBlock);
+        //Check staked balance above 0
+        require(LPstakedBalance[msg.sender] > 0);
+        //Repeat for every deposit made by user
+        for(uint i; i < LPStakes[msg.sender].deposits.length; i++) {
+            //get difference in block numbers from deposit block to current block
+            uint diff = (block.number.sub(LPStakes[msg.sender].depositTimes[i]));
+            //Clamp diff to maximum possible stake window
+            if(block.number > LPendBlock){
+                diff = diff.sub(block.number-LPendBlock);
+            }
+            //assign appropriate reward to user rewards[i]
+            //Uses calcReward() internal function
+            LPStakes[msg.sender].rewards[i] = calcReward(LPStakes[msg.sender].deposits[i])*diff;
+            //send amount of Xi stored in rewards[i] to user
+            Xi.transfer(msg.sender, LPStakes[msg.sender].rewards[i]);
+            //subtract reward amount from xiTokensLeft for next loop
+            xiTokensLeft = xiTokensLeft.sub(LPStakes[msg.sender].rewards[i]);
+            //subtract deposits[i] from user stakedBalance
+            LPstakedBalance[msg.sender] = LPstakedBalance[msg.sender].sub(LPStakes[msg.sender].deposits[i]);
+        }
+        //delete deposits[] and depositTimes[] to clear??
+        delete LPStakes[msg.sender].deposits;
+        delete LPStakes[msg.sender].depositTimes;
+        //Transfer POOH back to user
+        LPtoken.transfer(msg.sender, LPstakedBalance[msg.sender]);
+        //subtract from total staked
+        //Set user stakedBalance back to 0
+        LPstakedBalance[msg.sender] = 0;
+    }
+    
+    function calcReward(uint _reward) internal view returns (uint) {
+        uint div = LPStakeRewardCap/totalLPStaked;
+        uint mul = _reward.mul(div)/LPStakeTime; 
+        return mul;
+    }
+    
+    
     function burnUnclaimedXi() public onlyOwner {
         Xi.transfer(0x0000000000000000000000000000000000000000, Xi.balanceOf(address(this)));
+    }
+    
+    function burnUnclaimedPOOH() public onlyOwner {
+        POOH.transfer(0x0000000000000000000000000000000000000000, POOH.balanceOf(address(this)));
+    }
+    
+    function updateStakeTokenAddress(address _address) public onlyOwner {
+        poohAddy = _address;
+    }
+    
+    function updateRewardTokenAddress(address _address) public onlyOwner {
+        XiAddy = _address;
     }
     
     
